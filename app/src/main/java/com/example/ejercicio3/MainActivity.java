3835f3b3b50ccbba919562854af696fabdd1d9fc
Cristian <00086917@uca.edu.sv>
2019-03-15 20:47:53 -0600

    asignando id a botones correctamente

--- app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ejercicio3;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,9 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1.findViewById(R.id.button1);
-        button2.findViewById(R.id.button2);
-        button3.findViewById(R.id.button3);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
 
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
@@ -29,17 +30,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        int arrayRed[] = {rgb(240, 0, 0), rgb(215, 0, 0), rgb(190, 0, 0), rgb(150, 0, 0), rgb(120, 0, 0),
-                rgb(95, 0, 0)};
+        int[] arrayRed = {Color.rgb(240, 0, 0),
+                Color.rgb(215, 0, 0),
+                Color.rgb(190, 0, 0),
+                Color.rgb(150, 0, 0),
+                Color.rgb(120, 0, 0),
+                Color.rgb(95, 0, 0)};
 
-        int arrayBlue[] = {rgb(0, 0, 240), rgb(0, 0, 215), rgb(0, 0, 190), rgb(0, 0, 150), rgb(0, 0, 120),
-                rgb(0, 0, 95)};
+        int[] arrayBlue = {Color.rgb(0, 0, 240),
+                Color.rgb(0, 0, 215),
+                Color.rgb(0, 0, 190),
+                Color.rgb(0, 0, 150),
+                Color.rgb(0, 0, 120),
+                Color.rgb(0, 0, 95)};
 
-        int arrayGreen[] = {rgb(0, 240, 0), rgb(0, 215, 0), rgb(0, 190, 0), rgb(0, 150, 0), rgb(0, 120, 0),
-                rgb(0, 95, 0)};
+        int[] arrayGreen = {Color.rgb(0, 240, 0),
+                Color.rgb(0, 215, 0),
+                Color.rgb(0, 190, 0),
+                Color.rgb(0, 150, 0),
+                Color.rgb(0, 120, 0),
+                Color.rgb(0, 95, 0)};
 
 
         int viewId = v.getId();
+
         switch (viewId) {
             case R.id.button1: {
                 button1.setBackgroundColor(arrayRed[(int) (Math.random()*6)]);
@@ -54,6 +68,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             }
         }
-
     }
 }
